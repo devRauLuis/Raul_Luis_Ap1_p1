7e5735e9babbb027475e9041334a592363263511
Raul Luis <soyrauluis@outlook.com>
2022-02-01 23:09:23 -0400

    Add consulta boilerplate

---
+++ UI/Consultas/Consulta.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+
+namespace Raul_Luis_Ap1_p1.UI.Consultas;
+
+public partial class Consulta : Window
+{
+    public Consulta()
+    {
+        InitializeComponent();
+    }
+
+    private void BuscarButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+}
