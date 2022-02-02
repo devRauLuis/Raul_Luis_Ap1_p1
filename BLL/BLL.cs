cc27038b277629bbc8a6df4ea6273af83b62a4ca
Raul Luis <soyrauluis@outlook.com>
2022-02-01 22:17:49 -0400

    Add BLL

---
+++ BLL/BLL.cs
@@ -0,0 +1,14 @@
+using System;
+using Raul_Luis_Ap1_p1.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Raul_Luis_Ap1_p1.BLL
+{
+    public class BLL
+    {
+
+    }
+}
