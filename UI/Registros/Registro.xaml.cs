afdb2f3d9c599a7c3928942202893bce269beb64
Raul Luis <soyrauluis@outlook.com>
2022-02-01 23:09:10 -0400

    Add registro boilerplate

---
+++ UI/Registros/Registro.xaml.cs
@@ -0,0 +1,41 @@
+using System.Windows;
+
+namespace Raul_Luis_Ap1_p1.UI.Registros;
+
+public partial class Registro : Window
+{
+    public Registro()
+    {
+        InitializeComponent();
+    }
+
+    private void Cargar()
+    {
+    }
+
+    private void Limpiar()
+    {
+    }
+
+    private bool Validar()
+    {
+        return false;
+    }
+    private void BuscarButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void NuevoButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+
+    private void GuardarButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void EliminarButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+}
