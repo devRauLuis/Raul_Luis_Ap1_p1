7e62946553fa915e2ccf4931a7e9b7bea7e289a9
Raul Luis <soyrauluis@outlook.com>
2022-02-07 19:06:25 -0400

    Add Context

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-
+using Raul_Luis_Ap1_p1.Entidades;
 namespace Raul_Luis_Ap1_p1.DAL
 {
     public class Context : DbContext
     {
+
+        public DbSet<Producto> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite(@"Data Source=Data\Data.db");
+            optionsBuilder.UseSqlite(@"Data Source=Data\Productos.db");
         }
     }
 }
