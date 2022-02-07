16dbbc01584465a725e0e633ee66929383ec1f7f
Raul Luis <soyrauluis@outlook.com>
2022-02-07 19:15:34 -0400

    Add context

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -1,14 +1,13 @@
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Raul_Luis_Ap1_p1.Entidades;
-namespace Raul_Luis_Ap1_p1.DAL
+namespace Raul_Luis_Ap1_p1.DAL;
+public class Context : DbContext
 {
-    public class Context : DbContext
-    {
 
-        public DbSet<Producto> Productos { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite(@"Data Source=Data\Productos.db");
-        }
+    public DbSet<Producto> Productos { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=DATA/Productos.db");
     }
 }
