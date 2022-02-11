659df97187c72e0ed608c97e08ca8f711c04f156
Raul Luis <soyrauluis@outlook.com>
2022-02-11 13:24:50 -0400

    Update Consulta

--- UI/Consultas/cProductos.xaml.cs
+++ UI/Consultas/cProductos.xaml.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Windows;
 using System.Collections.Generic;
-
 using Raul_Luis_Ap1_p1;
 using Raul_Luis_Ap1_p1.Entidades;
 using Raul_Luis_Ap1_p1.BLL;
+
 namespace Raul_Luis_Ap1_p1.UI.Consultas;
 
 public partial class cProductos : Window
@@ -13,12 +13,14 @@ public partial class cProductos : Window
     {
         InitializeComponent();
         FiltroComboBox.SelectedIndex = 0;
+        ProductosDG.ItemsSource = ProductosBLL.GetList(p => true);
+
     }
 
     private void BuscarButton_Click(object sender, RoutedEventArgs e)
     {
         var lista = new List<Producto>();
-        if (string.IsNullOrWhiteSpace(CriterioTextBox.Text))
+        if (CriterioTextBox.Text is null)
         {
             lista = ProductosBLL.GetList(p => true);
         }
@@ -33,6 +35,7 @@ public partial class cProductos : Window
                         Utils.ShowErrorMessageBox("ID no valido");
                         break;
                     }
+
                     lista = ProductosBLL.GetList(p => p.ProductoId == id);
                     break;
                 case 1:
@@ -44,5 +47,4 @@ public partial class cProductos : Window
             ProductosDG.ItemsSource = lista;
         }
     }
-
 }
